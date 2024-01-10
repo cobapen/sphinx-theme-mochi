4371fe7641de21aa509038404defc6dc06e5d8fb
yamavol <yamavol@outlook.com>
2024-01-10 20:36:11 +0900

    update logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -85,7 +85,7 @@ mathjax_path = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'
 ## https://www.sphinx-doc.org/en/master/usage/configuration.html
 html_title = f"{project}"
 html_baseurl = ""
-html_logo = '_static/mochi.png'
+html_logo = '_static/logo.png'
 html_favicon = '_static/favicon.ico'
 # html_css_files = ''
 # html_js_files = ''
